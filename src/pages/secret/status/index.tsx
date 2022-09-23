7ac36bc0a0c172e0d3608edd0213539bc1cc7470
Dalton Craven <daltoncraven@proton.me>
2022-09-22 22:32:47 -0400

    add status

--- src/pages/secret/status/index.tsx
+++ src/pages/secret/status/index.tsx
@@ -6,7 +6,9 @@ const Status = () => (
   <div dir="rtl" lang="ar">
     <Page header={{ children: <h1>حالة</h1> }} isAuth>
       <h2>مرحبًا بك ، غني ، في مركزك الرقمي!</h2>
-      <p>الحالة ليست متاحة بعد.</p>
+      <p>
+        <strong>ها هي الحالة</strong>: ١ز٢٩ور٧٨٠٤٩٥٨٧٧١٠٧
+      </p>
       <Link href="/secret">العودة إلى الرئيسية</Link>
     </Page>
   </div>
